054197e262117492beb6a0f2e40890cac710928b
Douglas Rossi Melo <drm@ieee.org>
2023-06-14 17:50:58 -0300

    Implement adder tree in MADDER

--- rtl/madder.vhd
+++ rtl/madder.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 package madder_pkg is
   type t_VECT is array (natural range <>) of std_logic_vector;
+  type t_MATR is array (natural range <>, natural range <>) of std_logic_vector;
 end madder_pkg;
 
 library ieee;
@@ -12,16 +13,18 @@ use work.madder_pkg.all;
 
 entity madder is
   generic (
-    p_WIDTH : positive := 16;
-    p_COL   : positive := 16
+    p_WIDTH : positive := 8;
+    p_LEVEL : positive := 4
   );
   port (
-    i_P : in t_VECT (p_COL - 1 downto 0) (p_WIDTH - 1 downto 0);
-    o_S : out t_VECT (p_COL/2 - 1 downto 0)(p_WIDTH downto 0)
+    i_P : in t_VECT ((2 ** p_LEVEL) - 1 downto 0) (p_WIDTH - 1 downto 0);
+    o_S : out std_logic_vector(p_WIDTH + p_LEVEL - 1 downto 0)
   );
 end madder;
 
 architecture rtl of madder is
+
+  signal w_S : t_MATR((2 ** p_LEVEL) - 1 downto 0, p_LEVEL downto 0) (p_WIDTH + p_LEVEL - 1 downto 0);
 
   component sadder
     generic (
@@ -36,18 +39,28 @@ architecture rtl of madder is
 
 begin
 
-  col :
-  for i in 0 to p_COL/2 - 1 generate
-    sadder_inst : sadder
-    generic map(
-      p_WIDTH => p_WIDTH
-    )
-    port map(
-      i_A => i_P(i),
-      i_B => i_P(i + (p_COL/2)),
-      o_S => o_S(i)
-    );
+  row :
+  for j in p_LEVEL downto 1 generate
+    col :
+    for i in 0 to (((2 ** j)/2) - 1) generate
+      sadder_inst : sadder
+      generic map(
+        p_WIDTH => p_WIDTH + p_LEVEL - j
+      )
+      port map(
+        i_A(p_WIDTH + p_LEVEL - j - 1 downto 0) => w_S(2 * i, j)(p_WIDTH + p_LEVEL - j - 1 downto 0),
+        i_B(p_WIDTH + p_LEVEL - j - 1 downto 0) => w_S(2 * i + 1, j)(p_WIDTH + p_LEVEL - j - 1 downto 0),
+        o_S(p_WIDTH + p_LEVEL - j downto 0)     => w_S(i, j - 1)(p_WIDTH + p_LEVEL - j downto 0)
+      );
 
+    end generate;
   end generate;
+
+  start :
+  for i in 0 to 2 ** p_LEVEL - 1 generate
+    w_S(i, p_LEVEL)(p_WIDTH - 1 downto 0) <= i_P(i);
+  end generate;
+
+  o_S <= w_S(0, 0);
 
 end rtl;
