9c9900a15f7b58fbc2d216755f9dc75ba1e4fc20
Douglas Rossi Melo <drm@ieee.org>
2023-06-26 13:34:59 -0300

    Implement INGEN with MUX

--- rtl/ingen.vhd
+++ rtl/ingen.vhd
@@ -6,27 +6,28 @@ use work.dnu_pkg.all;
 entity ingen is
   generic (
     p_WIDTH : positive := 4;
-    p_LEVEL : positive := 4
+    p_LEVEL : positive := 8
   );
   port (
     i_CLK : in std_logic;
     i_RST : in std_logic;
     -- i_CTRL inputs
     i_SETK : in std_logic;
-    -- i_K    : in std_logic_vector(3 downto 0);
+    i_K    : in std_logic_vector(3 downto 0);
     i_SETP : in std_logic;
-    -- i_P    : in std_logic_vector(3 downto 0);
+    i_P    : in std_logic_vector(3 downto 0);
     i_RECT : in std_logic;
     i_GETK : in std_logic;
-    -- i_RAND : in std_logic_vector(9 downto 0);
+    i_RAND : in std_logic_vector(9 downto 0);
     o_ZERO : out std_logic;
     o_YMAX : out std_logic;
+    -- INGEN input
+    i_SEL   : in std_logic_vector(p_LEVEL - 1 downto 0);
+    i_A_GEN : in std_logic_vector(3 downto 0);
+    i_B_GEN : in std_logic_vector(3 downto 0);
     -- DNU output
     o_YDN : out std_logic_vector(3 downto 0);
-    o_YFP : out std_logic_vector(15 downto 0);
-    -- Input gen
-    i_A : in t_VECT ((2 ** p_LEVEL) - 1 downto 0) (3 downto 0);
-    i_B : in t_VECT ((2 ** p_LEVEL) - 1 downto 0) (3 downto 0)
+    o_YFP : out std_logic_vector(15 downto 0)
   );
 end ingen;
 
@@ -56,7 +57,6 @@ architecture rtl of ingen is
       -- DNU input
       i_A : in t_VECT ((2 ** p_LEVEL) - 1 downto 0) (3 downto 0);
       i_B : in t_VECT ((2 ** p_LEVEL) - 1 downto 0) (3 downto 0);
-
       -- DNU output
       o_YDN : out std_logic_vector(3 downto 0);
       o_YFP : out std_logic_vector(15 downto 0)
@@ -65,11 +65,11 @@ architecture rtl of ingen is
 
 begin
 
-  -- generate_inputs :
-  -- for i in 0 to 2 ** p_LEVEL - 1 generate
-  --   w_A (i) <= i_DATA(i);
-  --   w_B (i) <= i_DATA(2 ** p_LEVEL - 1 - i);
-  -- end generate;
+  generate_inputs :
+  for i in 0 to 2 ** p_LEVEL - 1 generate
+    w_A (i) <= i_A_GEN when to_integer(unsigned(i_SEL)) = i else (others => '0');
+    w_B (i) <= i_B_GEN when to_integer(unsigned(i_SEL)) = i else (others => '0');
+  end generate;
 
   dnu_inst : dnu
   generic map(
@@ -81,17 +81,17 @@ begin
     i_RST => i_RST,
     -- i_CTRL inputs
     i_SETK => i_SETK,
-    i_K    => i_A (0),
+    i_K    => i_K,
     i_SETP => i_SETP,
-    i_P    => i_B (0),
+    i_P    => i_P,
     i_RECT => i_RECT,
     i_GETK => i_GETK,
-    i_RAND => i_A (1) & i_A (2) & i_A (3)(1 downto 0),
+    i_RAND => i_RAND,
     o_ZERO => o_ZERO,
     o_YMAX => o_YMAX,
     -- DNU input
-    i_A => i_A,
-    i_B => i_B,
+    i_A => w_A,
+    i_B => w_B,
 
     -- DNU output
     o_YDN => o_YDN,
